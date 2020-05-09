e3a4563b9919451625551f2aa580a4f4ee106c56
marcus-elia <mselia@uvm.edu>
2020-05-09 10:11:17 -0400

    Added helper functions for gcd and modular inverse

--- precision_loser.py
+++ precision_loser.py
@@ -8,3 +8,33 @@ This file is for experimentally determining the precision loss, for all three
 (matrix, natural, efficient), hopefully up to at least Toom-10.
 """
 
+# ==============================
+#
+#         Math Functions
+#
+# ==============================
+def EEA(a, b):
+    """ Performs the extended Euclidean algorithm on a and b, returns
+        (gcd, u, v) such that au + bv = gcd"""
+    u = 1
+    g = a
+    x = 0
+    y = b
+    while y != 0:
+        q = g // y
+        t = g % y
+        s = u - q*x
+        u = x
+        g = y
+        x = s
+        y = t
+    v = (g - a*u) / b    
+    return (g, u, v)
+
+def inverse_mod(a, m):
+    """ Returns the inverse of a mod m if it exists"""
+    g,u,v = EEA(a, m)
+    if g != 1:
+        raise ValueError("({}, {}) != 1, can't invert".format(a,m))
+    else:
+        return ((u % m) + m) % m
