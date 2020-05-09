20892d1c0a52cc4a7fd91ef759e52a1aec391404
marcus-elia <mselia@uvm.edu>
2020-05-09 09:21:42 -0400

    Added precision loser file

---
+++ precision_loser.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 09:15:41 2020
+
+@author: Marcus
+
+This file is for experimentally determining the precision loss, for all three
+(matrix, natural, efficient), hopefully up to at least Toom-10.
+"""
+
