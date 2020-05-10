8593721bd653a22fbcb73d2b94bbc3864add8e1f
marcus-elia <mselia@uvm.edu>
2020-05-10 09:36:33 -0400

    Added multiplication function and efficient Toom-5 formulas

--- precision_loser.py
+++ precision_loser.py
@@ -8,6 +8,8 @@ This file is for experimentally determining the precision loss, for all three
 (matrix, natural, efficient), hopefully up to at least Toom-10.
 """
 
+import numpy as np
+
 # ========================================
 #
 #             Math Functions
@@ -28,7 +30,7 @@ def EEA(a, b):
         g = y
         x = s
         y = t
-    v = (g - a*u) / b    
+    v = (g - a*u) // b    
     return (g, u, v)
 
 def inverse_mod(a, m):
@@ -44,7 +46,7 @@ def split_powers_of_two(n):
     even_part = 1
     odd_part = n
     while odd_part % 2 == 0:
-        odd_part /= 2
+        odd_part //= 2
         even_part *= 2
     return (odd_part, even_part)
 
@@ -125,3 +127,180 @@ def evaluate_blocks_list_mod(blocks, values, m):
     for value in values:
         answer.append(evaluate_blocks_mod(blocks, value, m))
     return answer
+
+# ========================================
+#
+#             Interpolation
+#
+# ========================================
+
+def solve_for_coefficients_mod(n, r, m, formulas="efficient"):
+    """ This function handles the long formulas needed to explicitly find
+        the Toom formulas for Toom-2 up to Toom-6.
+        Enter 'matrix', 'natural', or 'efficient' for formulas"""
+    if formulas == "efficient":
+        if n == 5:
+            r0 = r[0]
+            r8 = r['infinity']
+
+            L = len(r0)
+
+            # the even temp variables
+
+            # start with E1, since it's not in a layer
+            E1 = [(((r[1][i] + r[-1][i]) % m)//2 - r0[i] - r8[i]) % m for i in range(L)]
+
+            # layer 1
+            E2 = [((r[2][i] + r[-2][i]) % m)//2 for i in range(L)]
+            for i in range(L):
+                E2[i] = (E2[i] - r0[i] - 256*r8[i]) % m
+                odd_part,even_part = split_powers_of_two(4)
+                denom_inv = inverse_mod(odd_part, m)
+                E2[i] = (E2[i] * denom_inv) % m
+                E2[i] = E2[i] //  even_part
+                E2[i] = (E2[i] - E1[i]) % m
+                odd_part,even_part = split_powers_of_two(3)
+                denom_inv = inverse_mod(odd_part, m)
+                E2[i] = (E2[i] * denom_inv) % m
+                E2[i] = E2[i] // even_part
+            E3 = [((r[3][i] + r[-3][i]) % m)//2 for i in range(L)]
+            for i in range(L):
+                E3[i] = (E3[i] - r0[i] - 6561*r8[i]) % m
+                odd_part,even_part = split_powers_of_two(9)
+                denom_inv = inverse_mod(odd_part, m)
+                E3[i] = (E3[i] * denom_inv) % m
+                E3[i] = E3[i] //  even_part
+                E3[i] = (E3[i] - E1[i]) % m
+                odd_part,even_part = split_powers_of_two(8)
+                denom_inv = inverse_mod(odd_part, m)
+                E3[i] = (E3[i] * denom_inv) % m
+                E3[i] = E3[i] // even_part
+            # the remaining even variables
+            r6 = [(E3[i] - E2[i]) % m for i in range(L)]
+            for i in range(L):
+                odd_part,even_part = split_powers_of_two(5)
+                denom_inv = inverse_mod(odd_part, m)
+                r6[i] = (r6[i] * denom_inv) % m
+                r6[i] = r6[i] // even_part
+            r4 = [(E2[i] - 5*r6[i] ) % m for i in range(L)]
+            r2 = [(E1[i] - r4[i] - r6[i] ) % m for i in range(L)]
+
+            # the odd temp variables
+
+            # start with O1, since it's not in a layer
+            O1 = [((r[1][i] - r[-1][i]) % m)//2 for i in range(L)]
+
+            # layer 1
+            O2 = [(r[2][i] - r[-2][i]) % m for i in range(L)]
+            for i in range(L):
+                odd_part,even_part = split_powers_of_two(4)
+                denom_inv = inverse_mod(odd_part, m)
+                O2[i] = (O2[i] * denom_inv) % m
+                O2[i] = O2[i] // even_part
+                O2[i] = (O2[i] - O1[i]) % m
+                odd_part,even_part = split_powers_of_two(3)
+                denom_inv = inverse_mod(odd_part, m)
+                O2[i] = (O2[i] * denom_inv) % m
+                O2[i] = O2[i] // even_part
+            O3 = [(r[3][i] - r[-3][i]) % m for i in range(L)]
+            for i in range(L):
+                odd_part,even_part = split_powers_of_two(6)
+                denom_inv = inverse_mod(odd_part, m)
+                O3[i] = (O3[i] * denom_inv) % m
+                O3[i] = O3[i] // even_part
+                O3[i] = (O3[i] - O1[i]) % m
+                odd_part,even_part = split_powers_of_two(8)
+                denom_inv = inverse_mod(odd_part, m)
+                O3[i] = (O3[i] * denom_inv) % m
+                O3[i] = O3[i] // even_part
+            O4 = [(r[4][i] - r0[i] - 16*r2[i] - 256*r4[i] - 4096*r6[i] - 65536*r8[i] ) % m for i in range(L)]
+            for i in range(L):
+                odd_part,even_part = split_powers_of_two(4)
+                denom_inv = inverse_mod(odd_part, m)
+                O4[i] = (O4[i] * denom_inv) % m
+                O4[i] = O4[i] // even_part
+                O4[i] = (O4[i] - O1[i]) % m
+                odd_part,even_part = split_powers_of_two(15)
+                denom_inv = inverse_mod(odd_part, m)
+                O4[i] = (O4[i] * denom_inv) % m
+                O4[i] = O4[i] // even_part
+
+            # layer 2
+            O5 = [(O3[i] - O2[i]) % m for i in range(L)]
+            for i in range(L):
+                odd_part,even_part = split_powers_of_two(5)
+                denom_inv = inverse_mod(odd_part, m)
+                O5[i] = (O5[i] * denom_inv) % m
+                O5[i] = O5[i] // even_part
+            O6 = [(O4[i] - O3[i]) % m for i in range(L)]
+            for i in range(L):
+                odd_part,even_part = split_powers_of_two(7)
+                denom_inv = inverse_mod(odd_part, m)
+                O6[i] = (O6[i] * denom_inv) % m
+                O6[i] = O6[i] // even_part
+
+            # the remaining odd variables
+            r7 = [(O6[i] - O5[i]) % m for i in range(L)]
+            for i in range(L):
+                odd_part,even_part = split_powers_of_two(12)
+                denom_inv = inverse_mod(odd_part, m)
+                r7[i] = (r7[i] * denom_inv) % m
+                r7[i] = r7[i] // even_part
+            r5 = [(O5[i] - 14*r7[i] ) % m for i in range(L)]
+            r3 = [(O2[i] - 5*r5[i] - 21*r7[i] ) % m for i in range(L)]
+            r1 = [(O1[i] - r3[i] - r5[i] - r7[i] ) % m for i in range(L)]
+
+            return (r0, r1, r2, r3, r4, r5, r6, r7, r8)
+            
+            
+# ========================================
+#
+#      The Multiplication Function
+#
+# ========================================
+def multiply(f, g, n, m, formulas="efficient"):
+    """ This multiplies f and g mod 2^m using Toom-n."""
+       
+    if len(f) != len(g):
+        raise ValueError("Can only multiply polys of the same length")
+            
+    fblocks = split(f, n)
+    gblocks = split(g, n)
+    
+    # the list of evaluating numbers
+    eval_list =  make_eval_list(n)
+    
+    # plug the numbers in
+    f_eval = evaluate_blocks_list_mod(fblocks, eval_list, 2**m)
+    g_eval = evaluate_blocks_list_mod(gblocks, eval_list, 2**m)
+    
+    # perform the recursive multiplication
+    r = {eval_list[i]:schoolbook_mod(f_eval[i], g_eval[i], 2**m)
+            for i in range(len(f_eval))}
+    
+    # Solve for the coefficients    
+    r_coefs = solve_for_coefficients_mod(n, r, 2**m, formulas)
+    
+    # recombination
+    k = int(np.ceil(len(f) / n))
+    prod = r_coefs[0][:k]
+    for j in range(1, 2*n-2):
+        prod = prod + [(r_coefs[j-1][k+i] + r_coefs[j][i]) % 2**m for i in range(k-1)]
+        prod = prod + [r_coefs[j][k-1]]
+
+    prod = prod + [(r_coefs[2*n-3][k+i] + r_coefs[2*n-2][i]) % 2**m for i in range(k-1)]
+
+    prod = prod + r_coefs[2*n-2][k-1:]
+
+    return prod[:2*len(f)-1]
+
+# ========================================
+#
+#            Precision Loss
+#
+# ========================================
+f = [1,4,3,4,5,0,7,4,6,4]
+g = [5,2,5,5,6,1,1,4,3,6]
+print(schoolbook_mod(f, g, 2**6))
+print(multiply(f, g, 5, 6))
+
