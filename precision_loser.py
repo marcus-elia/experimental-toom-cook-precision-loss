3012bd0f06e65fbed354b15dc2e353c4c48a82b3
marcus-elia <mselia@uvm.edu>
2020-05-10 09:03:11 -0400

    Copied in helper functions for modular Toom

--- precision_loser.py
+++ precision_loser.py
@@ -8,11 +8,11 @@ This file is for experimentally determining the precision loss, for all three
 (matrix, natural, efficient), hopefully up to at least Toom-10.
 """
 
-# ==============================
+# ========================================
 #
-#         Math Functions
+#             Math Functions
 #
-# ==============================
+# ========================================
 def EEA(a, b):
     """ Performs the extended Euclidean algorithm on a and b, returns
         (gcd, u, v) such that au + bv = gcd"""
@@ -47,3 +47,81 @@ def split_powers_of_two(n):
         odd_part /= 2
         even_part *= 2
     return (odd_part, even_part)
+
+# ========================================
+#
+#        Toom-Cook Helper Functions
+#
+# ========================================
+def schoolbook_mod(f, g, m):
+    """ Uses schoolbook multiplication to multiply f and g mod 
+        m. Returns the product as a list"""
+    d = len(f) + len(g) - 1
+    
+    # initialize a list of zeros
+    product = [0]*d
+    
+    # distribute through all possible combinations of coefficients
+    for i in range(len(f)):
+        for j in range(len(g)):
+            product[i + j] = (product[i+j] + f[i]*g[j]) % m
+    return product
+
+def split(f, num_blocks):
+    """ Splits the list f into num_blocks different blocks of equal size
+        If it doesn't divide evenly, we put zeros on the end of the last
+        block."""
+    blocks = []
+    copy_f = list(f)  # copy f so we don't ruin it!!!!!!!!
+    while len(copy_f) % num_blocks != 0:
+        copy_f.append(0)
+    block_length = len(copy_f) // num_blocks
+    index = 0
+    while index + block_length < len(copy_f):
+        blocks.append(copy_f[index:index+block_length])
+        index += block_length
+    blocks.append(copy_f[index:])
+    return blocks    
+
+def make_eval_list(n):
+    """ In Toom-n, this makes the list of numbers to plug in"""
+    if n == 2:
+        return (0, 1, 'infinity')
+    if n == 3:
+        return (0, 1, -1, 2, 'infinity')
+    if n == 4:
+        return (0, 1, -1, 2, -2, 3, 'infinity')
+    if n == 5:
+        return (0, 1, -1, 2, -2, 3, -3, 4, 'infinity')
+    if n == 6:
+        return (0, 1, -1, 2, -2, 3, -3, 4, -4, 5, 'infinity')
+    else:
+        raise ValueError("We haven't implemented Toom-" + str(n))
+
+def evaluate_blocks_mod(blocks, value, m):
+    """ blocks is a list of lists, each list is the coefficients of a
+        polynomial. But each list a coefficient. For example, if blocks is
+        [[1,2],[3,4],[5,6]] and value is -2, we return
+        [1,2] + [-6,-8] + [20,24] = [15, 18].  If the value is infinity,
+        we return the leading coefficient.
+        This does it mod m"""
+        
+    if value == 'infinity':
+        return blocks[-1]
+    
+    # initialize an empty list of the right length
+    answer = [0]*len(blocks[0])
+    
+    coefficient = 1
+    for i in range(len(blocks)):
+        for j in range(len(blocks[0])):
+            answer[j] = (answer[j] + coefficient*blocks[i][j]) % m
+        coefficient = (coefficient*value) % m
+    return answer
+
+def evaluate_blocks_list_mod(blocks, values, m):
+    """ Evaluates the blocks on a list of values, and returns a list"""
+    answer = []
+    for value in values:
+        answer.append(evaluate_blocks_mod(blocks, value, m))
+    return answer
